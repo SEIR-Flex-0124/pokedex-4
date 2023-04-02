c378ca61e7457b00b82fe53057e8b27b4227772a
Ronnie95 <t_daron@yahoo.com>
2023-04-02 12:14:23 -0400

    pics added

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 4500;
-const pokemonC = require("./models/pokemons");
+const pokemon = require("./models/pokemons");
 
 
 app.get("/", (req,res) => {
     res.send(`<h1>Test page</h1>`)
 });
 
+app.get("/pokemon", (req,res) => {
+    res.render(`index.ejs`, {pokemon})
+});
 
 
 
