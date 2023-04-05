1feee377cba4d8a20978da7801a223f3394998e2
Ronnie95 <t_daron@yahoo.com>
2023-04-05 15:39:49 -0400

    still updating

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 const PORT = 4500;
 const pokemon = require("./models/pokemons");
 
+const methodOverride = require('method-override');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended:false }));
+
+app.use(methodOverride('_method'));
+
 
 app.get("/", (req,res) => {
     res.send(`<h1>Test page</h1>`)
@@ -12,11 +22,19 @@ app.get("/pokemon", (req,res) => {
     res.render(`index.ejs`, {pokemon})
 });
 
+app.get("/pokemon/new", (req, res) => {
+    res.render(`new.ejs`);
+  });
+
 app.get("/pokemon/:id", (req, res) => {
     let poke = pokemon[req.params.id];
-    res.render(`show.ejs`, {poke})
+    res.render(`show.ejs`, {poke, idx: req.params.id})
 });
 
+app.get('/pokemon/:id/edit', (req, res) => {
+    const pokemonEdit = pokemon[req.params.id];
+    res.render(`edit.ejs`, {pokemonEdit, idx: req.params.id})
+});
 
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
