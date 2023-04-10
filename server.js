5982fc68f427450b1d5f39832f9095cef73c2d29
Ronnie95 <t_daron@yahoo.com>
2023-04-10 16:52:03 -0400

    changes made

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 4500;
 const pokemon = require("./models/pokemons");
-
 const methodOverride = require('method-override');
+//const { Router } = require("express");
+
 
 app.set('view engine', 'ejs');
 
@@ -31,10 +32,37 @@ app.get("/pokemon/:id", (req, res) => {
     res.render(`show.ejs`, {poke, idx: req.params.id})
 });
 
+app.post('/pokemon', (req, res) => {
+    let newPokemon = req.body;
+    pokemon.push(newPokemon)
+
+})
+
 app.get('/pokemon/:id/edit', (req, res) => {
     const pokemonEdit = pokemon[req.params.id];
     res.render(`edit.ejs`, {pokemonEdit, idx: req.params.id})
 });
 
+app.put('/pokemon/:id', (req, res) => {
+    let updatedPokemon = req.body;
+    pokemon[req.params.id] = updatedPokemon;
+    res.redirect(`/pokemon/${req.params.id}`)
+})
+
+app.get('/pokemon/:id/delete', (req, res) => {
+    const pokemonToDelete = pokemon[req.params.id];
+    res.render(`delete.ejs`, {pokemonToDelete, idx: req.params.id})
+})
+
+app.delete('/pokemon/:id/delete', (req, res) => {
+    const deletePokemon = pokemon[req.params.id];
+    pokemon.splice(req.params.id, 1);
+    res.redirect(`/pokemon`)
+})
+
+app.post('/:id', (req, res) => {
+    let pokemonUpdate = req.body
+
+})
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
